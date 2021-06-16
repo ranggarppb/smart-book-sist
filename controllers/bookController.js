f96e7a75c19a59f08fb86f43047617d98b1a4a0c
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-06-17 02:45:51 +0700

    finish user book info page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,9 +1,8 @@
-const { Book, Rack } = require("../models");
+const { Book, Rack, Review } = require("../models");
 
 class bookController {
   static getBooks(req, res) {
     let { notif } = req.query;
-    console.log(req.params);
     Book.findAll()
       .then((results) => {
         results = results.map((result) => result.dataValues);
@@ -25,7 +24,7 @@ class bookController {
       category: category,
       rack_id: +rack_id,
     })
-      .then(res.redirect("/book?notif=Berhasil menambahkan data buku"))
+      .then(res.redirect("/book/list?notif=Berhasil menambahkan data buku"))
       .catch((err) => res.send(err));
   }
   static getEditBookForm(req, res) {
@@ -62,7 +61,9 @@ class bookController {
       },
       { where: { id: +id } }
     )
-      .then(() => res.redirect(`/book?notif=Berhasil mengubah data buku ${id}`))
+      .then(() =>
+        res.redirect(`/book/list?notif=Berhasil mengubah data buku ${id}`)
+      )
       .catch((err) => res.render(err));
   }
   static deleteBook(req, res) {
@@ -72,8 +73,61 @@ class bookController {
         id: +id,
       },
     })
-      .then(() => res.redirect(`/book?notif=Berhasil mengubah data buku ${id}`))
+      .then(() =>
+        res.redirect(`/book/list?notif=Berhasil mengubah data buku ${id}`)
+      )
       .catch((err) => res.render(err));
+  }
+
+  static getBookScanner(req, res) {
+    res.render("book-scanner");
+  }
+  static getBookInfo(req, res) {
+    let { id } = req.params;
+    Book.findOne({
+      include: [
+        {
+          model: Rack,
+        },
+        {
+          model: Review,
+        },
+      ],
+      where: {
+        id: +id,
+      },
+    }).then((result) => {
+      let reviews = result.dataValues.Reviews.map(
+          (review) => review.dataValues.review
+        ),
+        ratings = result.dataValues.Reviews.map((review) => {
+          return review.dataValues.rating;
+        }),
+        averageRating = ratings.reduce((a, b) => a + b) / ratings.length;
+      res.render("book-info", {
+        book: result,
+        reviews: reviews,
+        averageRating: averageRating.toFixed(2),
+      });
+    });
+  }
+  static getAddReviewForm(req, res) {
+    let { id } = req.params;
+    res.render("add-book-review-form", { id: +id });
+  }
+  static addReview(req, res) {
+    let { review, rating } = req.body;
+    let { id } = req.params;
+    Review.create({
+      user_id: null,
+      book_id: +id,
+      review: review,
+      rating: rating,
+    })
+      .then(() =>
+        res.redirect(`/book/info/${id}?notif=Berhasil menambahkan review`)
+      )
+      .catch((err) => res.send(err));
   }
 }
 
--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -1,11 +1,16 @@
 const routers = require("express").Router();
 const bookController = require("../controllers/bookController");
 
-routers.get("/add", bookController.getAddBookForm);
-routers.post("/add", bookController.addBook);
-routers.get("/edit/:id", bookController.getEditBookForm);
-routers.post("/edit/:id", bookController.editBook);
-routers.get("/delete/:id", bookController.deleteBook);
-routers.get("/", bookController.getBooks);
+routers.get("/list/add", bookController.getAddBookForm);
+routers.post("/list/add", bookController.addBook);
+routers.get("/list/edit/:id", bookController.getEditBookForm);
+routers.post("/list/edit/:id", bookController.editBook);
+routers.get("/list/delete/:id", bookController.deleteBook);
+routers.get("/list", bookController.getBooks);
+
+routers.get("/info/:id", bookController.getBookInfo);
+routers.get("/info/create-review/:id", bookController.getAddReviewForm)
+routers.post("/info/create-review/:id", bookController.addReview)
+routers.get("/info", bookController.getBookScanner);
 
 module.exports = routers;
